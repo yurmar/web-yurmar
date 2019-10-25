c3ad2510124d305279d8dd60fbbe224e30495460
yurmar <yurmarchuk@gmail.com>
2019-10-25 15:05:23 +0800

    Create Header and Footer

--- resources/js/components/Main/Header/Header.js
+++ resources/js/components/Main/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="Header text-center">
             <div className="title">YurMar</div>
-            <div className="desc">Web developer - 3D print - Video editing</div>
+            <div className="desc">Web developing - 3D print - Video editing</div>
         </div>
     )
 }
