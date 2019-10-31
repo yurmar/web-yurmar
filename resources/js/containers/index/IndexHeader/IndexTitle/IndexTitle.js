083399a0cbc648d623460b12d4f66856e26fb93e
yurmar <yurmarchuk@gmail.com>
2019-10-31 12:39:04 +0800

    Fix IndexTitle and Topline

--- resources/js/containers/index/IndexHeader/IndexTitle/IndexTitle.js
+++ resources/js/containers/index/IndexHeader/IndexTitle/IndexTitle.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './IndexTitle.scss'
 
 export const IndexTitle = () => (
-    <div className="IndexTitle">
-        <div className="col-md-4">
+    <div className="IndexTitle col-md-8 offset-md-4 col-sm-8 offset-sm-4">
+        <div className="wrap">
             <div className="title">YurMar</div>
             <div className="tagLine">Делаю с душой и для души</div>
             <div className="desc">РАЗРАБАТЫВАЮ ДЛЯ СИЛЬНЫХ И ЛУЧШИХ КОМПАНИЙ И ЛИЧНОСТЕЙ. ПРЕВРАЩАЮ ИДЕИ В ОСЯЗАЕМОЕ</div>
